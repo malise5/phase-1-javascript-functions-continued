4a965c02878cb5022f60686adf17baa856add38a
Halkano <kudezx@gmail.com>
2022-08-18 22:32:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(job = "go to the office"){
+    return `This Monday, I will ${job}.`;
+
+}
+console.log(mondayWork("work from home"));
+
+
+const wrapAdjective = function(style ="*"){
+    return function (adjective = "special"){
+        return `You are ${style}${adjective}${style}!`
+    }
+}
